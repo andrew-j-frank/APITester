ed70ac713e33edd494f3467801a8c0411efc4a50
Andrew Frank <andrewjohnfrank@gmail.com>
2021-10-08 03:09:51 -0500

    Udpating this repo since a lot of things have changed for the api

--- APITester/Program.cs
+++ APITester/Program.cs
@@ -16,29 +16,42 @@ namespace APITester
             var client = new HttpClient()
             {
                 //BaseAddress = new Uri("http://localhost:26585/login")
-                BaseAddress = new Uri("https://localhost:44346/login")
+                BaseAddress = new Uri("https://localhost:44346/maintenance/signup")
             };
             var sendObject = new
             {
                 /*user_id = 3,
+                phone = "1234567890",
                 property_id = 1,
-                current_address = "124 hello st",
-                monthly_income = 10.0,
+                current_address = "124 hello 3st",
+                monthly_income = 15.0,
                 comment = "pls give me appartment",
                 status = "not looked at"*/
                 /*property_id = 1,
-                priority = 1,
-                description = "leaking roof",
-                status = "not fixed",*/
+                user_id = 3,
+                manager_id = 1,
+                priority = 10,
+                description = "dfsdsf boing",
+                status = "not fixed"*/
                 /*address = "123 hello st",
-                rent = 10.0,
-                renter = 3,
-                manager = 2,
-                balance = 0.0,*/
-                full_name = "Andrew Frank5",
-                username = "afrank",
-                password = "password5",
-                is_manager = true
+                rent = 12.53,
+                manager_id = 1,
+                manager_name = "manager1",
+                balance = 10.65,
+                details = new
+                {
+                    sqft = 1002,
+                    bedrooms = 3,
+                    bathrooms = 2,
+                    description = "nice"
+                }*/
+                /*full_name = "Andrew Frank4",
+                username = "afrank4",
+                password = "password4",
+                is_manager = false*/
+                //balance = 10.32
+                //property_id = 4
+                //status = "fixed"
             };
             var content = new StringContent(JsonConvert.SerializeObject(sendObject).ToString(),Encoding.UTF8, "application/json");
             var request = new HttpRequestMessage()
